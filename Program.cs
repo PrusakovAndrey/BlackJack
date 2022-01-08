b7da367de0d5526ab824118549486c6dadb1b9b6
Andrey <prusakov.andrey@gmail.com>
2022-01-08 19:15:49 +0300

    2init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Игра BlackJack, с использованием двух колод");
+
+string[] value = new string [] {"2","3","4","5","6","7","8","9","10","Валет","Дама","Король","Туз"};
+string[] suit = new string[] {"Черви", "Буби", "Крести", "Пики"};
+int x = new Random().Next(0, value.Length);
+int y = new Random().Next(0, suit.Length);
+string card = value[x] + " " + suit[y];
+Console.WriteLine(card);
